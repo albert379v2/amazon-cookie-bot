01309d922c83536a967cbcc8910a0a97c1b353dc
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-17 01:04:15 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -77,6 +77,8 @@ class MailTM:
         for _ in range(20):
             await asyncio.sleep(8)
             try:
+                msgs_data = self.session.get(f"{self.api}/messages").json()
+                send_log(str(msgs_data))
                 msgs = self.session.get(f"{self.api}/messages").json()['hydra:member']
                 if msgs:
                     msg_id = msgs[0]['id']
