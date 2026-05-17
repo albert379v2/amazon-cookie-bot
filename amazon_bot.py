c7ddbc31497dff340728c2e52075d8d78a0c3eff
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-17 11:23:59 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -55,26 +55,26 @@ class MailTM:
         self.token = ""
 
     def get_account(self):
-        try:
-            send_log("📡 Obteniendo dominios MailTM")
+    try:
+        send_log("📡 Obteniendo dominios MailTM")
 
         r = self.session.get(f"{self.api}/domains", timeout=20)
         data = r.json()
 
-            send_log(f"DOMAINS RESPONSE: {data}")
+        send_log(f"DOMAINS RESPONSE: {data}")
 
         domain = data['hydra:member'][0]['domain']
 
         self.address = f"zeus{random.randint(1000,9999)}@{domain}"
 
-            send_log(f"📧 Creando cuenta: {self.address}")
+        send_log(f"📧 Creando cuenta: {self.address}")
 
         res = self.session.post(f"{self.api}/accounts", json={
             "address": self.address,
             "password": self.password
         }, timeout=25)
 
-            send_log(f"ACCOUNT RESPONSE: {res.text}")
+        send_log(f"ACCOUNT RESPONSE: {res.text}")
 
         auth = self.session.post(f"{self.api}/token", json={
             "address": self.address,
@@ -83,6 +83,7 @@ class MailTM:
 
         self.token = auth['token']
         self.session.headers.update({"Authorization": f"Bearer {self.token}"})
+
         send_log("✅ MailTM listo")
 
         return self.address
