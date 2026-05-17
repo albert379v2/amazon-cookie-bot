443972b47527640743b686129391343c5d3693f6
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-16 23:49:17 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -134,7 +134,7 @@ async def create_amazon():
             send_log(f"🚀 Creando: `{email}`")
             send_log("E7 - Entrando a Amazon")
             await page.goto(
-    "https://www.amazon.com/ap/register",
+    "https://www.amazon.com.mx/ap/signin?openid.return_to=https%3A%2F%2Fwww.amazon.com.mx%2F%3F_encoding%3DUTF8%26ref_%3Dnavm_hdr_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=anywhere_v2_mx&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0",
     wait_until="domcontentloaded",
     timeout=120000
 )
