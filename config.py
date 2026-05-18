9c25e965b7ddd743e353a66464b6377da5c32c4d
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-18 10:59:55 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 import os
 
+# === CONFIGURACIÓN ===
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 AC_KEY = os.getenv("AC_KEY")
@@ -9,6 +10,12 @@ AC_KEY = os.getenv("AC_KEY")
 PROXY_ADDR = os.getenv("PROXY_ADDR")
 PROXY_USER = os.getenv("PROXY_USER")
 PROXY_PASS = os.getenv("PROXY_PASS")
+
+print("TOKEN:", TOKEN)
+print("CHAT_ID:", CHAT_ID)
+print("PROXY_ADDR:", PROXY_ADDR)
+print("PROXY_USER:", PROXY_USER)
+print("PROXY_PASS:", PROXY_PASS)
 
 PROXY_CONFIG = {
     "server": f"http://{PROXY_ADDR}",
