5d34fe0598f0a954b3f69212fb056b50633cbb7a
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-18 11:31:57 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -1,4 +1,5 @@
 from config import *
+from utils.logger import *
 import time
 import os
 import json
@@ -15,43 +16,9 @@ from playwright.async_api import async_playwright
 # === CONFIGURACIÓN ===
 
 bot = telebot.TeleBot(TOKEN)
+init_logger(bot)
 
-DEBUG_DIR = "debug"
-os.makedirs(DEBUG_DIR, exist_ok=True)
 
-def safe_name(name: str) -> str:
-    return (
-        name.replace("/", "_")
-            .replace(" ", "_")
-            .replace(":", "_")
-            .replace("?", "_")
-            .replace("=", "_")
-    )
-    
-async def take_screenshot(page, step: str):
-    path = os.path.join(DEBUG_DIR, f"{safe_name(step)}.png")
-
-    await page.screenshot(
-        path=path,
-        full_page=True
-    )
-
-    return path
-
-def send_screenshot(path: str, caption: str):
-    with open(path, "rb") as img:
-        bot.send_photo(CHAT_ID, img, caption=caption)
-async def debug(page, step: str):
-    try:
-        path = await take_screenshot(page, step)
-        send_screenshot(path, f"📸 {step}")
-    except Exception as e:
-        print(f"Screenshot error: {e}")
-
-def send_log(msg):
-    print(msg)
-    try: bot.send_message(CHAT_ID, f"🤖 {msg}", parse_mode="Markdown")
-    except: pass
 #iniciamos detección de captcha
 async def detect_canvas_captcha(page):
 
