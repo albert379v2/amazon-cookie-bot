68c839e5965663faeb5abb3f65a06dbf03c051f6
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-18 16:48:38 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -9,33 +9,9 @@ import requests
 import base64
 import time
 from playwright.async_api import async_playwright
-
+from config import *
 
 # === CONFIGURACIÓN ===
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-AC_KEY = os.getenv("AC_KEY")
-
-PROXY_ADDR = os.getenv("PROXY_ADDR")
-PROXY_USER = os.getenv("PROXY_USER")
-PROXY_PASS = os.getenv("PROXY_PASS")
-
-print("TOKEN:", TOKEN)
-print("CHAT_ID:", CHAT_ID)
-print("PROXY_ADDR:", PROXY_ADDR)
-print("PROXY_USER:", PROXY_USER)
-print("PROXY_PASS:", PROXY_PASS)
-
-PROXY_CONFIG = {
-    "server": f"http://{PROXY_ADDR}",
-    "username": PROXY_USER,
-    "password": PROXY_PASS
-}
-
-REQUESTS_PROXIES = {
-    "http": f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_ADDR}/",
-    "https": f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_ADDR}/"
-}
 
 bot = telebot.TeleBot(TOKEN)
 
