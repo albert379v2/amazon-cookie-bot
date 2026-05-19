cb80fbca4becc6b4afdf42e4d8f7c4d8167a0c23
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-19 17:27:43 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -38,8 +38,8 @@ async def detect_canvas_captcha(page):
 #captura del captcha
 async def capture_captcha(page):
 
-    canvas = page.locator("#captcha-container")
-    await canvas.screenshot(path="captcha.png")
+    captcha = page.locator("#captcha-container")
+    await captcha.screenshot(path="captcha.png")
     
     return "captcha.png"
 
