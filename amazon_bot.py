4f9e3a84e3982c1a67c7d40c2d5ed380b59a1be4
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-20 16:29:28 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -213,7 +213,7 @@ async def create_amazon():
             send_log("E7 - Entrando a Amazon")
             #await debug(page, "0create_mail")
             await page.goto(
-    "https://www.amazon.com.mx/ap/signin?openid.return_to=https%3A%2F%2Fwww.amazon.com.mx%2F%3F_encoding%3DUTF8%26ref_%3Dnavm_hdr_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=anywhere_v2_mx&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0",
+    "https://www.amazon.com.mx/cpe/yourpayments/settings/manageoneclick",
     wait_until="domcontentloaded",
     timeout=120000
 )
