c73b56864a464d9175f04260fb5eb58ac17646ff
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-20 18:43:45 -0600

    Update login.py

--- handlers/login.py
+++ handlers/login.py
@@ -1,5 +1,11 @@
 from utils import send_log
 
+async def handle_signin(page, email):
+
+    await page.fill("#ap_email_login", email)
+
+    await page.click("#continue")
+
 async def handle_login_password(page):
 
     send_log("⚠️ Correo ya registrado")
