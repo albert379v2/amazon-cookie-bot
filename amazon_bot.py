b25e2c94a2fc200ba24c55393c245699f1906409
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-21 00:15:32 -0600

    Update amazon_bot.py

--- amazon_bot.py
+++ amazon_bot.py
@@ -311,6 +311,7 @@ async def create_amazon():
 
 
                 elif state == "CAPTCHA_DETECTED":
+                    await page.wait_for_timeout(1000)
                     send_log("🧩 CAPTCHA DETECTADO")
                     await page.wait_for_timeout(1200)
                     captcha_type = await detect_captcha_type(page)
