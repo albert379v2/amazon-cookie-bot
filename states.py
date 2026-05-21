5d74e7cf36c58fcc0ec8d7c1273850e90ea20839
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-20 20:10:04 -0600

    Update states.py

--- states.py
+++ states.py
@@ -1,23 +1,21 @@
 async def detect_state(page):
 
-    text = await page.locator("body").inner_text()
-
-    text = text.lower()
-
-    # SIGNIN
-    if "iniciar sesión o crear cuenta" in text:
-        return "SIGNIN"
+    text = (await page.locator("body").inner_text()).lower()
 
     # LOGIN EXISTENTE
     if "amazon contraseña" in text:
         return "LOGIN_PASSWORD"
 
+    # SIGNIN
+    if "ingresa el número de celular" in text:
+        return "SIGNIN"
+
     # NUEVA CUENTA
-    if "parece que eres nuevo" in text:
+    if "parece que eres nuevo en amazon" in text:
         return "REGISTER_INTRO"
 
     # FORMULARIO
-    if "nombre y apellido" in text:
+    if "crear cuenta" in text and "nombre y apellido" in text:
         return "REGISTER_FORM"
 
     # CAPTCHA CANVAS
@@ -28,7 +26,7 @@ async def detect_state(page):
     if "utiliza las flechas" in text:
         return "CAPTCHA_ORBIT"
 
-    # OTP
+    # OTP EMAIL
     if "verifica la dirección de correo electrónico" in text:
         return "OTP_EMAIL"
 
