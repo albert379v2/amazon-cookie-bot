5531ad581b64537a7ea0b670fd33bed899997838
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-22 14:12:49 -0600

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,6 @@
 from utils import send_log
 
 async def detect_state(page):
-    if await page.locator("#cvf-aamation-challenge-iframe").count() > 0:
-        return "CAPTCHA_DETECTED"
 
     # LOGIN
     if await page.locator("#ap_email_login").count() > 0:
@@ -12,35 +10,39 @@ async def detect_state(page):
     if await page.locator("text=Proceder a crear una cuenta").count() > 0:
         return "REGISTER_INTRO"
 
-
-
-
+    # DETECTAR PUZZLE INTERNO
     for frame in page.frames:
+
         try:
+
             text = (await frame.locator("body").inner_text()).lower()
+
             if "hazla coincidir" in text:
                 return "CAPTCHA_ORBIT_GAME"
-            
+
             if "utiliza las flechas" in text:
                 return "CAPTCHA_ORBIT_GAME"
+
             if "envíe" in text:
                 return "CAPTCHA_ORBIT_GAME"
+
+            if "arrastra" in text:
+                return "CAPTCHA_ORBIT_GAME"
+
         except:
             pass
 
-
-
-    
+    # CAPTCHA GENERAL
+    if await page.locator("#cvf-aamation-challenge-iframe").count() > 0:
+        return "CAPTCHA_DETECTED"
 
     # REGISTER FORM
     if await page.locator("input[name='customerName']").count() > 0:
         return "REGISTER_FORM"
-        
 
     # OTP EMAIL
     if await page.locator("#cvf-input-code").count() > 0:
         return "OTP_EMAIL"
-
 
     # CAPTCHA CANVAS
     if await page.locator("canvas").count() > 0:
