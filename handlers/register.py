c4cf0ca0f210e79877b0641526cb68b62642ae0f
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-05-22 19:17:00 -0600

    Update register.py

--- handlers/register.py
+++ handlers/register.py
@@ -1,14 +1,18 @@
 async def handle_register_intro(page):
+    send_log("✍️ entrando pagina de registro o login")
 
     await page.click("#intention-submit-button")
     await page.wait_for_timeout(1000)
+    send_log("✍️ saliendo registro login")
 
 
 async def handle_register_form(page):
+    send_log("✍️ ingresando registro")
 
     await page.fill("#ap_customer_name", "Jhonatan aldama")
-
+    send_log("✍️ Insertando nombre")
     await page.fill("#ap_password", "Admin.2026.!")
 
     await page.click("#continue")
+    send_log("✍️ trrmino ingresar información")
     await page.wait_for_timeout(4000)
